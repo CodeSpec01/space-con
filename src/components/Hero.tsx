ee01cd72c7b5a361238a7c1ff9633b0b889b9735
CodeSpec <online.codespec@gmail.com>
2024-12-10 23:11:20 +0530

    hero resp 1.0

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,42 +2,40 @@ import { useEffect } from "react";
 import Navbar from "./Navbar";
 
 const Hero = () => {
+  useEffect(() => {
+    const parallax_el = document.querySelectorAll(".parallax");
 
-    useEffect(() => {
-      const parallax_el = document.querySelectorAll(".parallax");
+    let xValue = 0;
+    let yValue = 0;
 
-      let xValue = 0;
-      let yValue = 0;
+    window.addEventListener("mousemove", (e) => {
+      xValue = e.clientX - window.innerWidth / 2;
+      yValue = e.clientY - window.innerHeight / 2;
 
-      window.addEventListener("mousemove", (e) => {
-        xValue = e.clientX - window.innerWidth / 2;
-        yValue = e.clientY - window.innerHeight / 2;
+      parallax_el.forEach((ele) => {
+        const el = ele as HTMLElement;
+        let speedX = parseFloat(el.dataset.speedx!);
+        let speedY = parseFloat(el.dataset.speedy!);
+        let speedZ = parseFloat(el.dataset.speedz!);
 
-        parallax_el.forEach((ele) => {
+        let mapSide =
+          parseFloat(getComputedStyle(el).left) < window.innerWidth / 2
+            ? 1
+            : -1;
+        let zValue =
+          (e.clientX - parseFloat(getComputedStyle(el).left)) *
+          mapSide *
+          0.2 *
+          speedZ;
 
-            const el = ele as HTMLElement;
-          let speedX = parseFloat(el.dataset.speedx!);
-          let speedY = parseFloat(el.dataset.speedy!);
-          let speedZ = parseFloat(el.dataset.speedz!);
-
-          let mapSide =
-            parseFloat(getComputedStyle(el).left) < window.innerWidth / 2
-              ? 1
-              : -1;
-          let zValue =
-            (e.clientX - parseFloat(getComputedStyle(el).left)) *
-            mapSide *
-            0.2 *
-            speedZ;
-
-          el.style.transform = `translateX(${
-            -xValue * speedX * 0.5
-          }px) translateY(${
-            -yValue * speedY * 0.5
-          }px) perspective(2300px) translateZ(${zValue}px)`;
-        });
+        el.style.transform = `translateX(${
+          -xValue * speedX * 0.5
+        }px) translateY(${
+          -yValue * speedY * 0.5
+        }px) perspective(2300px) translateZ(${zValue}px)`;
       });
-    }, []);
+    });
+  }, []);
 
   return (
     <>
@@ -47,7 +45,7 @@ const Hero = () => {
           className="absolute h-full w-full bg-center object-cover -z-50"
           src="/hero-bg.png"
         ></img>
-        <div className="h-screen w-full absolute top-0 left-0 vignette z-40"></div>
+        {/* <div className="h-[100svh] w-full absolute top-0 left-0 vignette z-40"></div> */}
         <div className="absolute w-full h-full top-0 left-0 overflow-hidden flex justify-center items-center ">
           <img
             data-speedx={"0.05"}
@@ -67,35 +65,35 @@ const Hero = () => {
             data-speedx={"0.04"}
             data-speedy={"0.04"}
             data-speedz={"1"}
-            className="parallax top-[53%] left-[26%] w-[9vw] z-10 "
+            className="parallax top-[40%] left-[10%] w-[20vw] md:top-[53%] md:left-[26%] md:w-[9vw] z-10 "
             src="/planet-1.png"
           ></img>
           <img
             data-speedx={"0.02"}
             data-speedy={"0.02"}
             data-speedz={"1"}
-            className="parallax top-[10%] left-[4%] w-[20vw] "
+            className="parallax top-[12%] -left-[8%] w-[40vw] md:top-[10%] md:left-[4%] md:w-[20vw] "
             src="/planet-2.png"
           ></img>
           <img
             data-speedx={"0.06"}
             data-speedy={"0.06"}
             data-speedz={"1"}
-            className="parallax top-[47%] left-[59%] w-[6vw] "
+            className="parallax top-[47%] left-[59%] w-[10vw] md:top-[47%] md:left-[59%] md:w-[6vw] "
             src="/planet-4.png"
           ></img>
           <img
             data-speedx={"0.09"}
             data-speedy={"0.09"}
             data-speedz={"1"}
-            className="parallax top-[12%] right-[7%] w-[7vw] "
+            className="parallax top-[12%] right-[7%] w-[14vw] md:top-[12%] md:right-[7%] md:w-[7vw] "
             src="/planet-5.png"
           ></img>
           <img
             data-speedx={"0.08"}
             data-speedy={"0.08"}
             data-speedz={"1"}
-            className="parallax top-[57%] right-[18%] w-[17vw] "
+            className="parallax top-[63%] right-0 w-[32vw] md:top-[57%] md:right-[18%] md:w-[17vw] "
             src="/planet-7.png"
           ></img>
           <img
@@ -109,7 +107,7 @@ const Hero = () => {
             data-speedx={"0.04"}
             data-speedy={"0.04"}
             data-speedz={"1"}
-            className="parallax top-[57%] left-[2%] w-[20vw] "
+            className="parallax top-[57%] left-[2%] md:w-[20vw] w-[30vw] "
             src="/astronaut-2.png"
           ></img>
           <img
@@ -120,14 +118,14 @@ const Hero = () => {
             src="/astronaut-3.png"
           ></img>
         </div>
-        <div className="h-screen w-full absolute top-0 left-0 flex justify-center items-center flex-col pb-[20%]">
-        <h1 className="font-orbitron text-white text-[12vw] md:text-[8vw] text-center align-middle border-b-2 md:leading-[9vw] z-40 ">
-          <strong>SPACECON</strong>
-        </h1>
-        <h2 className="font-mono text-white text-[4vw]  text-center align-middle z-40">
-          NAKSHATRA x TDS
-        </h2>
-      </div>
+        <div className="h-screen w-full absolute top-0 left-0 flex justify-center items-center flex-col pb-[35vh] md:pb-[20vh]">
+          <h1 className="font-orbitron text-white text-[12vw] md:text-[8vw] text-center align-middle border-b-2 md:leading-[9vw] z-40 ">
+            <strong>SPACECON</strong>
+          </h1>
+          <h2 className="font-mono text-white text-[4vw]  text-center align-middle z-40">
+            NAKSHATRA x TDS
+          </h2>
+        </div>
       </div>
     </>
   );
