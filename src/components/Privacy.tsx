bdcc284f6130ab33009cb6f28921cb2ddb711b44
CodeSpec <online.codespec@gmail.com>
2024-12-12 16:16:15 +0530

    panel update 2.0

--- src/components/Privacy.tsx
+++ src/components/Privacy.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Privacy = () => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
       <h2 className="text-white font-orbitron text-[3vw] text-center">
         Privacy Policy
       </h2>
+      <Link to={"/space-con-secret-admin-panel"}>Don't Click Me</Link>
     </div>
   );
 };
