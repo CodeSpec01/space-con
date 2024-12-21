728fcb3b8caa41ace2f666a03a562734d92ba70b
CodeSpec <online.codespec@gmail.com>
2024-12-21 22:03:48 +0530

    deployment 8.0

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,7 +1,5 @@
-// import Card from "./Card";
 import DownArrow from "./DownArrow";
 import EventDisplay from "./EventDisplay";
-// import { EventsList } from "./EventsList";
 import Meteors from "./Meteors";
 import { VelocityScroll } from "./ScrollVelocity";
 
@@ -38,13 +36,9 @@ const Events = ({ data }: { data: Events[] }) => {
         default_velocity={-4}
         className=" text-center font-bold tracking-[-0.02em] text-[13vw] md:text-[5vw] leading-[5vh] bg-[#2f2f2f] py-6"
       />
-      {/* <h3 className="py-[8vh] text-[8vw] text-[#6356fd] ">Coming Soon</h3> */}
       <div className="">
         <EventDisplay data={data} />
       </div>
-      {/* <div className="hidden">
-        <EventsList data={data} />
-      </div> */}
       <a href="/events" className="rounded-3xl bg-gray-800 py-2 px-3 invert hover:invert-0 text-[4vw] md:text-[1.5vw] mb-10 ">
         All Events &#8599;
       </a>
