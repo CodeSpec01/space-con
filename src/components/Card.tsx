9bbf2b14789175f9a6daa210b09f0f157b60f680
CodeSpec <online.codespec@gmail.com>
2025-01-29 16:02:39 +0530

    update Card component to handle optional event data and improve rendering logic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,44 +1,50 @@
 import { Event } from "../constants";
 
-const Card = ({ data, className }: { data: Event, className?: string }) => {
+const Card = ({ data, className }: { data?: Event; className?: string }) => {
   return (
     <div
       className={`w-[70vw] md:w-[30vw] bg-[#1f1f1f] p-6 pt-1 rounded-xl flex flex-col gap-3 justify-between ${className} `}
     >
       <div>
         <h3 className="text-[#6356fd] text-center text-[7vw] font-semibold py-1 md:text-[3vw] ">
-          {data.name.toUpperCase()}
+          {data!.name.toUpperCase()}
         </h3>
         <img
-          src={data.image}
+          src={data!.image}
           alt="Event Image"
           className="rounded-lg w-full object-cover m-auto "
         />
       </div>
-      <p className="font-extralight text-[3.5vw] md:text-[1vw]">{data.description}</p>
+      <p className="font-extralight text-[3.5vw] md:text-[1vw]">
+        {data!.description}
+      </p>
       <div className="md:text-[1.2vw] text-[5vw] ">
         <span>
-          <strong className="text-[#6356fd]">DATE :</strong> {data.date}
+          <strong className="text-[#6356fd]">DATE :</strong> {data!.date}
+        </span>
+        <br />
+        {data?.startTime && data.endTime ? (
+          <>
+            <span>
+              <strong className="text-[#6356fd]">TIMING</strong> :{" "}
+              {data.startTime} - {data.endTime}
+            </span>
+            <br />
+          </>
+        ) : null}
+        <span>
+          <strong className="text-[#6356fd]">PRIZE :</strong> {data!.prize}
         </span>
         <br />
         <span>
-          <strong className="text-[#6356fd]">TIMING</strong> : {data.startTime}{" "}
-          - {data.endTime}
-        </span>
-        <br />
-        <span>
-          <strong className="text-[#6356fd]">PRIZE :</strong> {data.prize}
-        </span>
-        <br />
-        <span>
-          <strong className="text-[#6356fd]">VENUE :</strong> {data.venue}
+          <strong className="text-[#6356fd]">VENUE :</strong> {data!.venue}
         </span>
         <br />
       </div>
       <div className="w-full flex flex-col md:flex-row items-center mt-1 gap-2 md:gap-0 justify-around">
         <a
-          href={data.ruleBook}
-          className="flex items-center gap-2 bg-green-500/30 hover:bg-[#333333] w-fit p-3 rounded-xl py-2 "
+          href={data!.ruleBook}
+          className="flex items-center gap-2 bg-green-500/30 hover:bg-[#333333] w-fit p-3 rounded-xl py-2 transition-all duration-300"
           onClick={() => {
             console.log(data);
           }}
@@ -49,8 +55,8 @@ const Card = ({ data, className }: { data: Event, className?: string }) => {
           </span>
         </a>
         <a
-          href={data.link}
-          className="flex items-center gap-2 bg-black/50 hover:bg-[#333333] w-fit p-3 rounded-xl py-2 "
+          href={data!.link}
+          className="flex items-center gap-2 bg-black/50 hover:bg-[#333333] w-fit p-3 rounded-xl py-2 transition-all duration-300"
         >
           <span className="custom-arrow-pointer">Register Now!</span>
           <span className="custom-arrow-pointer bg-gray-800 rounded-full w-7 h-7 flex items-center justify-center text-center invert ">
--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@ export type Event = {
   description: string;
   venue: string;
   date: string;
-  startTime: string;
+  startTime?: string;
   link: string;
   image: string;
-  endTime: string;
+  endTime?: string;
   prize: string;
   ruleBook: string;
 };
@@ -17,12 +17,12 @@ export const mainEvents: Event[] = [
     description:
       "The Hackathon at SpaceCon is an immersive and dynamic 24-hour innovation marathon, designed to bring together brilliant minds and challenge them to ideate, design and prototype solutions using cutting edge Generative AI, Machine learning and Web 3.0/Blockchain. Participants will have to collaborate to solve real-world problems in the realm of space technology and its application.",
     venue: "Coming Soon",
-    date: "2nd day spacecon, 24 jan",
-    startTime: "Coming Soon",
-    link: "https://aviralgaur.vercel.app/about",
+    date: "22nd - 23rd Feb",
+    startTime: "8:00 am",
+    link: "https://unstop.com/o/moUA7bT?lb=OjRJ5Man",
     image: "/ctrlalthack.png",
-    endTime: "Coming Soon",
-    prize: "Coming Soon",
+    endTime: "2:00 pm",
+    prize: "1.5 Lakhs",
     ruleBook:
       "https://drive.google.com/file/d/1lfh6MgsGQZKtfFPBUVxKasK9_ug0W3zj/view",
   },
@@ -30,13 +30,11 @@ export const mainEvents: Event[] = [
     name: "SkyScape",
     description:
       "Astrophotography is an event that invites participants to capture the beauty of the celestial wonders. Whether you're using a mobile phone , a professional camera or a telescope, this event provides a wonderful opportunity for everyone to showcase their photography skills.",
-    venue: "Coming Soon",
-    date: "6th January",
-    startTime: "Coming Soon",
+    venue: "Online",
+    date: "29th Jan - 10th Feb",
     link: "https://unstop.com/events/skyscape-spacecon-2025-netaji-subhas-university-of-technology-nsut-delhi-1378287",
     image: "/skyscape.png",
-    endTime: "Coming Soon",
-    prize: "Coming Soon",
+    prize: "6K",
     ruleBook:
       "https://drive.google.com/file/d/1o_wKaOvfM9njH_oM4ngnCyTg0ofXvbew/view",
   },
@@ -48,7 +46,7 @@ export const allEvents: Event[] = [
     description:
       "The Hackathon at SpaceCon is an immersive and dynamic 24-hour innovation marathon, designed to bring together brilliant minds and challenge them to ideate, design and prototype solutions using cutting edge Generative AI, Machine learning and Web 3.0/Blockchain. Participants will have to collaborate to solve real-world problems in the realm of space technology and its application.",
     venue: "Coming Soon",
-    date: "2nd day spacecon, 24 jan",
+    date: "Coming Soon",
     startTime: "Coming Soon",
     link: "https://aviralgaur.vercel.app/about",
     image: "/ctrlalthack.png",
@@ -61,13 +59,11 @@ export const allEvents: Event[] = [
     name: "SkyScape",
     description:
       "Astrophotography is an event that invites participants to capture the beauty of the celestial wonders. Whether you're using a mobile phone , a professional camera or a telescope, this event provides a wonderful opportunity for everyone to showcase their photography skills.",
-    venue: "Coming Soon",
-    date: "6th January",
-    startTime: "Coming Soon",
+    venue: "Online",
+    date: "29th Jan - 10th Feb",
     link: "https://unstop.com/events/skyscape-spacecon-2025-netaji-subhas-university-of-technology-nsut-delhi-1378287",
     image: "/skyscape.png",
-    endTime: "Coming Soon",
-    prize: "Coming Soon",
+    prize: "Rs 6000",
     ruleBook:
       "https://drive.google.com/file/d/1o_wKaOvfM9njH_oM4ngnCyTg0ofXvbew/view",
   },
